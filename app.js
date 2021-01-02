9fa3282727e05c4435d3850849e2b255f9d506de
Rao Hassan <raohassan4212@gmail.com>
2021-01-02 14:41:35 +0500

    task of chapter 1 & 2 complete

---
+++ app.js
@@ -0,0 +1,65 @@
+//                                      chapter: 01
+
+
+// // task: 1
+// alert("Welcome using JS alert");
+
+// // task: 2
+// alert("Error! Please Enter a Valid Password");
+
+// // task: 3
+// alert(" Welcome to Js Land... \r\n Happy Coding");
+
+// // task: 4
+// alert("Welcome to JS Land...");
+// alert(" Happy Coding! \r\n Prevent this page from creating additional dialogs.");
+
+// // task: 5
+// console.log(alert("Hello I can JS through my web browser console"));
+
+// // task: 6
+// alert("Use of alert in your new/exsting HTML & CSS Project");
+
+// task: 7
+// task 7 is done in index.html
+
+
+
+//                                     chapter: 02
+
+
+// // task: 1
+// var userName;
+
+// // task: 2
+// var myName = "Rao Hassan";
+
+// // task: 3
+// let message = "Hello World";
+// alert(message);
+
+// // task" 4
+// var name = "Jhone Doe";
+// var age = 15;
+// var course = "Web and Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(course);
+
+// // task: 5
+// var x = " PIZZA \r\n PIZZ \r\n PIZ \r\n PI \r\n P";
+// alert(x);
+
+// // task: 6
+// var email = "examle@example.com";
+// alert("My email address is " + email);
+
+// // task: 7
+// var message = "A smarter \r\n way to learn JavaScript";
+// alert(" I am trying to learn from the Book " + message);
+
+// // task: 8 
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// task: 9
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
